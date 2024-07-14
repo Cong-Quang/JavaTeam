825f83c2becb90ec844bfe15dd47ad7aed711e6d
Nguyễn Công Quang <98932936+Cong-Quang@users.noreply.github.com>
2024-07-14 17:27:37 +0700

    Update Main.java

--- example/demo/src/main/java/com/example/Main.java
+++ example/demo/src/main/java/com/example/Main.java
@@ -7,22 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Tuan tuan = new Tuan();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap a: ");
-        tuan.a = scanner.nextInt();
-        System.out.println("Nhap b: ");
-        tuan.b = scanner.nextInt();
-        System.out.println(Color.cRed + "Tong: " + tuan.Cong(tuan.a, tuan.b));
-        System.out.println(Color.cGreen + "Tru: " + tuan.Tru(tuan.a, tuan.b));
-        System.out.println(Color.cYellow + "Nhan: " + tuan.Nhan(tuan.a, tuan.b));
-        if(tuan.Chia(tuan.a, tuan.b) == 0){
-            System.out.println(Color.cCyan + "Khong the chia cho 0");
-        }
-        else{
-            System.out.println(Color.cCyan + "Chia: " + tuan.Chia(tuan.a,tuan.b));
-        }
-        Color.reset();
-        scanner.close();
+    {
+        
     }
 }
