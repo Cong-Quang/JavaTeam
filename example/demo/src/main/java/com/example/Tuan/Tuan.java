72e2eb021267eb2bbe47b52364c66d8cf92e2ec3
Anh-Tuan0105 <huynhngocanhtuan9a9@gmail.com>
2024-07-14 18:29:00 +0700

    Sửa Bài Lần n

--- example/demo/src/main/java/com/example/Tuan/Tuan.java
+++ example/demo/src/main/java/com/example/Tuan/Tuan.java
@@ -1,21 +1,43 @@
 package com.example.Tuan;
 
+import java.util.Scanner;
+import com.example.accsset.Color;
 public class Tuan {
-    public int a, b;
-    public int Cong(int a, int b){
-        return a+b;
+    private int a, b;
+    public void tinhtoan(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        this.a = scanner.nextInt();
+        System.out.println("Nhap b: ");
+        this.b = scanner.nextInt();
+        scanner.close(); 
+        double c = Chia();
+        System.out.println(Color.cRed + "Cong: " + Cong());
+        System.out.println(Color.cGreen + "Tru: " + Tru());
+        System.out.println(Color.cYellow + "Nhan: " + Nhan());
+        if(Chia()==0){
+            System.out.println(Color.cCyan + "Khong the chia cho 0");
+        }
+        else{
+            System.out.printf(Color.cCyan + "Chia: %.2f", c);
+        }
+        Color.reset();
     }
-    public int Tru(int a, int b){
-        return a-b;
+
+    private int Cong(){
+        return this.a + this.b;
     }
-    public long Nhan(int a, int b){
-        return a*b;
+    private int Tru(){
+        return this.a - this.b;
     }
-    public double Chia(int a, int b){
-        if(b==0){
+    private long Nhan(){
+        return this.a * this.b;
+    }
+    private double Chia(){
+        if(this.b==0){
             //System.out.println("Khong the chia cho 0");
             return 0;
         }
-        return (double)a/b;
+        return (double)this.a / this.b;
     }
 }
