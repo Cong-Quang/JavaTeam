cd3170b8f330cde09d73ef296349d25c036d86ce
Cong-Quang <nguyencongquang02082003@gmail.com>
2024-07-26 17:00:55 +0700

    xxxx

--- example/demo/src/main/java/com/example/Quang/Console.java
+++ example/demo/src/main/java/com/example/Quang/Console.java
@@ -1,41 +1,47 @@
 package com.example.Quang;
- 
+
 import com.example.accsset.Color;
-public class Console extends Color { // extends là kế thừa những gì đã có ở trong lớp Color.  nếu là private thì nó không lấy ra được
-                                        // chỉ public th nhá. cấu trúc     [class mới] extends [class khác]
+
+public class Console extends Color { // extends là kế thừa những gì đã có ở trong lớp Color. nếu là private thì nó
+                                     // không lấy ra được
+                                     // chỉ public th nhá. cấu trúc [class mới] extends [class khác]
 
     static String ANSI_ESC = "\033[";
+
     // in ra theo vị trí
-    public static void print(String s,int row, int col) {
+    public static void print(String s, int row, int col) {
         System.out.print(ANSI_ESC + row + ";" + col + "H" + s + ANSI_ESC + "0m");
         Color.reset();
     }
 
-    public static void println(String s,Color color) {
+    public static void println(String s, Color color) {
         System.out.println(s); // thêm ln là xuống hàng
     }
+
     // hàm loader của teo mới viết ( đọc hiểu nếu hok hiểu có thể coppy)
-    // vì sao t có thể gọi các thuộc tính của hàm color mà không cần color.[tới] thì kéo lên đọc lại note ở hàm Console extends Color
+    // vì sao t có thể gọi các thuộc tính của hàm color mà không cần color.[tới] thì
+    // kéo lên đọc lại note ở hàm Console extends Color
     public static void loader(int total) throws InterruptedException {
-        
+
         for (int i = 0; i <= total; i++) {
             printProgressBar(i, total);
-
+            // test máy mới
             // Giả lập công việc với sleep
             Thread.sleep(100);
         }
         System.out.println(cRed + " [Hoàn thành] ");
-        reset(); 
+        reset();
     }
+
     private static void printProgressBar(int current, int total) {
-        int barLength = 100; 
+        int barLength = 100;
         int progress = (int) ((double) current / total * barLength);
         StringBuilder bar = new StringBuilder("[");
 
         // Tạo phần thanh tiến trình đã hoàn thành
         for (int i = 0; i < progress; i++) {
             bar.append("=");
-            
+
         }
         // Tạo mũi tên chỉ thị vị trí hiện tại
         if (progress < barLength) {
